b05f7043d071c794002a8967bca42948b6998c6d
Levente Varga <varga.levente@happyempire.hu>
2023-02-25 13:24:37 +0100

    The client working with remote API and ChatServer

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ChatServer
         static void Main(string[] args)
         {
             _users = new List<Client>();
-            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7890);
+            _listener = new TcpListener(IPAddress.Parse("0.0.0.0"), 7890);
             _listener.Start();
 
             while (true)
@@ -38,6 +38,32 @@ namespace ChatServer
                     user.ClientSocket.Client.Send(broadcastPacket.GetPacketBytes());
                 }
             }
+        }
+
+        public static void BroadcastMessage(string message)
+        {
+            foreach(var user in _users)
+            {
+                var msgPacket = new PacketBuilder();
+                msgPacket.WriteOpCode(5);
+                msgPacket.WriteMessage(message);
+                user.ClientSocket.Client.Send(msgPacket.GetPacketBytes());
+            }
+        }
+        
+        public static void BroadcastDisconnect(string uid)
+        {
+            var disconnectedUser = _users.Where(x => x.UID.ToString() == uid).FirstOrDefault();
+            _users.Remove(disconnectedUser);
+
+            foreach(var user in _users)
+            {
+                var broadcastPacket = new PacketBuilder();
+                broadcastPacket.WriteOpCode(10);
+                broadcastPacket.WriteMessage(uid);
+                user.ClientSocket.Client.Send(broadcastPacket.GetPacketBytes());
+            }
+            BroadcastMessage($"({disconnectedUser.Username} Disconnected!)");
         }
     }
 }
