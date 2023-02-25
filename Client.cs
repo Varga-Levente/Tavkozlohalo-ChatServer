b77e4faf56d4e51887fdd58103a189579161fcb0
Levente Varga <varga.levente@happyempire.hu>
2023-02-25 01:31:20 +0100

    ServerBase

---
+++ Client.cs
@@ -0,0 +1,27 @@
+﻿using ChatServer.Net.IO;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    internal class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        PacketReader _packetReader;
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+
+            _packetReader = new PacketReader(client.GetStream()); // create new PacketReader instance
+            var opcode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {Username}");
+        }
+
+    }
+}
